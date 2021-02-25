6b597e0e7e0497f12f5d79a639ab26d87e47da49
andemar3 <71730527+andemar3@users.noreply.github.com>
2021-02-25 11:30:42 -0800

    Implemented CREATE for playermanager and contentcreator

--- contentcreator.js
+++ contentcreator.js
@@ -2,13 +2,99 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-//add functions here
+//get all Locations
+	function getLocations(res, mysql, context, complete){
+		mysql.pool.query("SELECT locationId as id, locationName FROM Locations", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.locations  = results;
+            complete();
+        });
+    }
+	
+//get all Locations
+	function getQuests(res, mysql, context, complete){
+		mysql.pool.query("SELECT questId as id, questName FROM Quests", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.quests  = results;
+            complete();
+        });
+    }
+
+//insert new Location
+	router.post('/location', function(req, res){
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO Locations (locationName) VALUES (?)";
+        var inserts = [req.body.locationName];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/contentcreator');
+            }
+        });
+    });
+	
+//insert new Quest
+	router.post('/quest', function(req, res){
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO Quests (questName, questLocation, statRequired, statMinimum) VALUES (?, ?, ?, ?)";
+        var inserts = [req.body.locationName];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/contentcreator');
+            }
+        });
+    });
+	
+//insert new Item
+	router.post('/item', function(req, res){
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO Items (itemName, questRewardedFrom, statBoosted, statBoostAmount) VALUES (?, ?, ?, ?)";
+        var inserts = [req.body.locationName];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/contentcreator');
+            }
+        });
+    });
+
 
 //renders initial page view
     function renderContentCreator(req, res){
 	var context = {};
-	res.render('contentcreator', context)
-    }
+	var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+		//get Locations and Quests for dropdowns
+        getLocations(res, mysql, context, complete);
+        getQuests(res, mysql, context, complete);
+		//render page when getLocations and getQuests have finished
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('contentcreator', context);
+            }
+		}
+	}
 	
     router.get('/', renderContentCreator);
 	
--- playermanager.js
+++ playermanager.js
@@ -4,6 +4,25 @@ module.exports = function(){
 
 //add functions here
 
+//handles create player form
+	router.post('/', function(req, res){
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO Players (playerName) VALUES (?)";
+		var context = {};
+        var inserts = [req.body.playerName];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+				context.message = ["New player created!"];
+                res.render('playermanager', context)
+            }
+        });
+    });
+	
 //renders initial page view
     function renderPlayerManager(req, res){
 	var context = {};
