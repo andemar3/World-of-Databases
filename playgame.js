4dc9e57a8e6d0025a53c64377ac216c80b7932de
Manju Kuah <kuahm@oregonstate.edu>
2021-02-25 23:21:09 -0800

    updated select queries. attempted to display selec

--- playgame.js
+++ playgame.js
@@ -3,7 +3,16 @@ module.exports = function(){
     var router = express.Router();
 
 //add functions here
-
+	function getTable(res, mysql, context, complete){
+		mysql.pool.query("SELECT Players.playerName, Locations.locationName, Items.itemName FROM Players JOIN PlayersItems on PlayersItems.playerID = Players.playerID JOIN Locations on Locations.locationID = Players.currentLocationID JOIN Items on Items.itemID = PlayersItems.itemID;", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.locations  = results;
+            complete();
+        });
+    }
 //renders initial page view
     function renderPlayGame(req, res){
 	var context = {};
--- search.js
+++ search.js
@@ -3,6 +3,29 @@ module.exports = function(){
     var router = express.Router();
 
 //add functions here
+//get all quest with items
+	function getQuestandItems(res, mysql, context, complete){
+		mysql.pool.query("SELECT questRewardedFrom, itemName FROM Items;", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.items  = results;
+            complete();
+        });
+    }
+	
+//get all Quests
+	function getQuests(res, mysql, context, complete){
+		mysql.pool.query("SELECT questId as id, questName FROM Quests", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.quests  = results;
+            complete();
+        });
+    }
 
 //renders initial page view
     function renderSearch(req, res){
