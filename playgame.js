ff2b65cf5928f14fc987e2c4ff4b5b141f5d8d14
andemar3 <71730527+andemar3@users.noreply.github.com>
2021-03-15 13:00:13 -0700

    Fixed updateplayer display

--- playgame.js
+++ playgame.js
@@ -43,7 +43,7 @@ module.exports = function(){
 
     //update data
 function getPlayer(res, mysql, context, playerID, complete){
-    var sql = "SELECT Players.playerID, Players.playerName, Players.currentQuest, Players.currentLocationID, Players.playerHealth, Players.playerMagic, Players.strengthStat, Players.intelligenceStat, Players.defenceStat, Locations.locationName, Quests.questName, Items.itemName, Items.itemID, PlayersItems.playerID, PlayersItems.itemID FROM Players JOIN Locations on Locations.locationID = Players.currentLocationID JOIN Quests on Quests.questID = Players.currentQuest JOIN PlayersItems on PlayersItems.playerID = Players.playerID JOIN Items on Items.itemID = PlayersItems.itemID WHERE Players.playerID = ?";
+    var sql = "SELECT Players.playerID, Players.playerName, Players.currentQuest, Players.currentLocationID, Players.playerHealth, Players.playerMagic, Players.strengthStat, Players.intelligenceStat, Players.defenceStat FROM Players WHERE Players.playerID = ?";
 	console.log(playerID);
     var inserts = [playerID];
     mysql.pool.query(sql, inserts, function(error, results, fields){
@@ -57,10 +57,54 @@ function getPlayer(res, mysql, context, playerID, complete){
     });
 }
 
+function getPlayerLocation(res, mysql, context, playerID, complete){
+    var sql = "SELECT Locations.locationName, Locations.locationID FROM Locations JOIN Players ON Players.currentLocationID = Locations.locationID WHERE Players.playerID = ?";
+	console.log(playerID);
+    var inserts = [playerID];
+    mysql.pool.query(sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+        context.playerLocation = results[0];
+        complete();
+    });
+}
+
+function getPlayerQuest(res, mysql, context, playerID, complete){
+    var sql = "SELECT Quests.questName, Quests.questID FROM Quests JOIN Players ON Players.currentQuest = Quests.questID WHERE Players.playerID = ?";
+	console.log(playerID);
+    var inserts = [playerID];
+    mysql.pool.query(sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+		console.log(context.playerQuest);
+        context.playerQuest = results[0];
+        complete();
+    });
+}
+
+function getPlayerItems(res, mysql, context, playerID, complete){
+    var sql = "SELECT Items.itemName, Items.itemID FROM Items JOIN PlayersItems ON Items.itemID = PlayersItems.itemID JOIN Players ON Players.playerID = PlayersItems.playerID WHERE Players.playerID = ? ";
+	console.log(playerID);
+    var inserts = [playerID];
+    mysql.pool.query(sql, inserts, function(error, results, fields){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+        context.playerItems = results;
+        console.log(context.playerItems);
+        complete();
+    });
+}
+
 	router.put('/:playerID', function(req, res){
         var mysql = req.app.get('mysql');
         var sql = "UPDATE Players SET currentLocationID = ? WHERE playerID = ?";
-        var inserts = [req.body.LocationID, req.params.playerID];
+        var inserts = [req.body.locationID, req.params.playerID];
         sql = mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
                 console.log(error)
@@ -82,9 +126,12 @@ function getPlayer(res, mysql, context, playerID, complete){
         getPlayer(res, mysql, context, req.params.playerID, complete);
         getlocations(res, mysql, context, complete);
         getQuests(res, mysql, context, complete);
+		getPlayerLocation(res, mysql, context, req.params.playerID, complete);
+		getPlayerQuest(res, mysql, context, req.params.playerID, complete);
+		getPlayerItems(res, mysql, context, req.params.playerID, complete)
         function complete(){
             callbackCount++;
-            if(callbackCount >= 3){
+            if(callbackCount >= 6){
                 console.log(context);
                 res.render('updateplayer', context);
             }
